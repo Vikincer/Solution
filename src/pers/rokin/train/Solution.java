a0f157721cb4e672caeb451c25301d484d78cbe2
Vikincer <vikinc@163.com>
2022-09-07 17:59:39 +0800

    09-06

--- src/pers/rokin/train/Solution.java
+++ src/pers/rokin/train/Solution.java
@@ -1660,6 +1660,27 @@ class Solution {
         return Integer.toBinaryString(
                 Integer.parseInt(a, 2) + Integer.parseInt(b, 2)
         );
-    }
 
+    }
+    /*整数的 数组形式 num是按照从左到右的顺序表示其数字的数组。
+例如，对于 num = 1321 ，数组形式是 [1,3,2,1] 。
+给定 num ，整数的 数组形式 ，和整数 k ，返回 整数 num + k 的 数组形式 。*/
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> res = new ArrayList<Integer>();
+        int n = num.length;
+        for (int i = n - 1; i >= 0; --i) {
+            int sum = num[i] + k % 10;
+            k /= 10;
+            if (sum >= 10) {
+                k++;
+                sum -= 10;
+            }
+            res.add(sum);
+        }
+        for (; k > 0; k /= 10) {
+            res.add(k % 10);
+        }
+        Collections.reverse(res);
+        return res;
+    }
 }
